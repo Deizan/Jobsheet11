e7c4c2b8e635578eb82b671875a5fda889106b33
Deizan <deizanfakhri7@gmail.com>
2025-11-20 08:10:06 +0700

    percobaan 4

--- SIAKAD16.java
+++ SIAKAD16.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class SIAKAD16 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner scanner = new Scanner(System.in);
         int[][] nilai = new int[4][3];
 
