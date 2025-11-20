07e2849c58db4760a9841a55e0e2d7a2b9ae018e
Deizan <deizanfakhri7@gmail.com>
2025-11-20 08:28:42 +0700

    Modifikasi percobaan ke 1 no.5

--- BioskopModifikasi16.java
+++ BioskopModifikasi16.java
@@ -1,4 +1,4 @@
-public class Bioskop16 {
+public class BioskopModifikasi16 {
     public static void main(String[] args)  {
         String[][] penonton = new String[4][4];
         penonton[0][0] = "Amin";
@@ -13,10 +13,10 @@ public class Bioskop16 {
         System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);   
         System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
-        System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
+        System.out.println();        
+        System.out.println("Total baris: " + penonton.length);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton[i].length);
+        }
     }
 }
