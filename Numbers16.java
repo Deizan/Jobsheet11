fe571af06c62745753dc43a577f8772c84c2a8e6
Deizan <deizanfakhri7@gmail.com>
2025-11-20 08:09:27 +0700

    percobaan 3

--- Numbers16.java
+++ Numbers16.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Numbers16 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner scanner = new Scanner (System.in);
         int[][] myNumbers = new int[3][ ];
         myNumbers[0] = new int[5];
