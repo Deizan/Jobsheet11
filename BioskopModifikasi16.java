1277274ff0496543e946d70d406c8bdd04ea56bd
Deizan <deizanfakhri7@gmail.com>
2025-11-20 09:45:41 +0700

    pertanyaan no.6 percobaan ke 1

--- BioskopModifikasi16.java
+++ BioskopModifikasi16.java
@@ -16,13 +16,11 @@ public class BioskopModifikasi16 {
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
   
         System.out.println(penonton.length);
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
         }
-        System.out.println();
-        System.out.println("Penonton pada baris ke-3: ");
-        for (String i : penonton[2]) {
-            System.out.println(i);
+        
         }
     }
-}
+
